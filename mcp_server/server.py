183d60b3ff9edd0ffee49ee7c3e1ddff874f3ef8
PFT\Barry_Kao <barry_kao@perfectcorp.com>
2025-09-10 18:38:16 +0800

    fix mcp get ebugcode function

--- mcp_server/server.py
+++ mcp_server/server.py
@@ -2,45 +2,42 @@
 import json
 import requests
 from fastmcp import FastMCP
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 mcp = FastMCP("Real Estate MCP")  # Remove host/port from constructor
 #mcp.run()
-
-
+load_dotenv()
 
 @mcp.tool("add")  # 定義一個工具
 def add(a: int, b: int) -> int:
     """Add two numbers together."""
     return a + b
-'''
-@mcp.tool  # 另一個範例工具
-def multiply(a: int, b: int) -> int:
-    """Multiply two numbers."""
-    return a * b
-'''
+
 
 @mcp.tool("ebugcode")  # 另一個範例工具
-def get_ebugcodes_by_key_word(keyword:str) :
-    url = "<url>"
+def get_ebugcodes_by_key_word(keyword:str, page_index: int, months_ago: int):
+    url = os.getenv("EPF_URL")
     """query ebugs by key word"""
-
+    token = os.getenv("TOKEN")
     headers = {
-        "Authorization": "Bearer " + "<token>",
+        "Authorization": "Bearer " + token,
         "Content-Type": "application/json",
     }
 
-    payload = {"queryModel":"TSR.EbugSearch","pageSize":25,"pageIndex":1,"isDesc":True,"sortBy":"CreateTime","conditions":
-        [{"field":"CreateTime","range":{"gte":"2025-03-30","lte":"2025-09-09"}},
+    format_string = "%Y-%m-%d %H:%M:%S"
+    current_dt = datetime.now()
+    past_dt = current_dt - relativedelta(months=months_ago)
+
+    payload = {"queryModel":"TSR.EbugSearch","pageSize":25,"pageIndex":page_index,"isDesc":True,"sortBy":"CreateTime","conditions":
+        [{"field":"CreateTime","range":{"gte":past_dt.strftime(format_string),"lte":current_dt.strftime(format_string)}},
          {"field":"IsValid","termNot":{"query":"false"}},
          {"field":"Status","termNot":{"querys":["Close","NAB","Postpone","Wont Fix","Won't Fix","NotReproducible","Cancelled"]}},
          {"field":"ShortDescription","match":{"query":keyword}}]}
     response = requests.post(url, headers=headers, json=payload)
 
-    #if response.status_code == 200:
-    #    print("Success:" + response.text)
-    #else:
-    #    print(f"Failed with status code: {response.status_code}")
-    #    print("Error:" + response.text)
     return response.text
 
 
